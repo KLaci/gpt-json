d66db69de301d41e65cd9080c284a6e023b45f22
Laszlo Kiss <laszlo.kiss27@gmail.com>
2023-08-07 11:17:03 +0200

    Initial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,138 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+import { Configuration, OpenAIApi } from 'openai';
+import {
+  ArraySchema,
+  ObjectSchema,
+  SchemaFieldDescription,
+  SchemaInnerTypeDescription,
+  SchemaObjectDescription,
+} from 'yup';
+
+export interface GPTJSONOptions {
+  apiKey: string;
+}
+
+export interface GPTJSONRequest {
+  model: string;
+  request: string;
+  schema: ObjectSchema<any> | ArraySchema<any, any, any, any>;
+}
+
+function indentation(depth: number): string {
+  return '  '.repeat(depth);
+}
+
+function objectSchemaDescription(
+  schema: SchemaObjectDescription,
+  depth: number
+): string {
+  const serializedDescription = `${indentation(depth)}{
+${Object.entries(schema.fields)
+  .map(([f, d]) => fieldSchemaDescription(f, d, depth + 1))
+  .join('\n')}
+${indentation(depth)}}`;
+
+  return serializedDescription;
+}
+
+function arraySchemaDescription(
+  schema: SchemaObjectDescription,
+  depth: number
+): string {
+  const arraySchema = schema as SchemaInnerTypeDescription;
+
+  const serializedDescription = `${indentation(depth)}[
+${innerSchemaDescription(arraySchema.innerType as any, depth + 1)}
+${indentation(depth)}]`;
+
+  return serializedDescription;
+}
+
+function fieldSchemaDescription(
+  fieldName: string,
+  fieldSchemaDescription: SchemaFieldDescription,
+  depth: number
+): string {
+  const fieldSchema = fieldSchemaDescription as any;
+  const serializedDescription = `${indentation(
+    depth
+  )}${fieldName}: ${innerSchemaDescription(fieldSchema, depth + 1)}${
+    fieldSchema?.meta?.description
+      ? ` // ${fieldSchema?.meta?.description}`
+      : ''
+  }`;
+
+  return serializedDescription;
+}
+
+function innerSchemaDescription(
+  schema: SchemaObjectDescription,
+  depth: number
+): string {
+  if (schema.type === 'object') {
+    return objectSchemaDescription(schema, depth);
+  } else if (schema.type === 'array') {
+    return arraySchemaDescription(schema, depth);
+  }
+
+  return schema.type;
+}
+
+export function generateSchemaDescription(schema: ObjectSchema<any>): string {
+  const describedSchema = schema.describe();
+
+  const depth = 0;
+  const serializedDescription = innerSchemaDescription(describedSchema, depth);
+
+  return serializedDescription;
+}
+
+class GPTJSON {
+  private apiKey: string;
+
+  constructor({ apiKey }: GPTJSONOptions) {
+    this.apiKey = apiKey;
+  }
+
+  async executeRequest({
+    model,
+    request,
+    schema,
+  }: GPTJSONRequest): Promise<Record<string, unknown>> {
+    const config = new Configuration({
+      apiKey: this.apiKey,
+    });
+
+    const openai = new OpenAIApi(config);
+
+    const response = await openai.createChatCompletion({
+      model: model,
+      messages: [
+        {
+          role: 'user',
+          content: `${request}
+      Do not include any explanations, only provide a  RFC8259 compliant JSON response following this format without deviation.
+      ${generateSchemaDescription(schema as any)}`,
+        },
+      ],
+    });
+
+    const respMessage =
+      response.data.choices?.[0]?.message?.content?.trim() ?? '';
+
+    if (respMessage === '') {
+      throw new Error('No response from OpenAI');
+    }
+
+    const parsedResponse = JSON.parse(respMessage);
+
+    const isValid = await schema.isValid(parsedResponse);
+
+    if (!isValid) {
+      throw new Error('Invalid response from OpenAI');
+    }
+
+    return parsedResponse;
+  }
+}
+
+export default GPTJSON;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,69 @@
-import { myPackage } from '../src';
+import { array, number, object, string } from 'yup';
+import GPTJSON, { generateSchemaDescription } from '../src';
+
+const client = new GPTJSON({
+  apiKey: process.env.OPENAI_API_KEY as string,
+});
 
 describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+  describe('first test', () => {
+    // it('should work', async () => {
+    //   const response = await client.executeRequest({
+    //     model: 'gpt-4',
+    //     request: 'Give me some basic data about Mount Everest',
+    //     schema: object({
+    //       height: number().required(),
+    //     }),
+    //   });
 
-      const result = myPackage(message);
+    //   expect(response).toEqual({
+    //     height: 8848,
+    //   });
+    // });
 
-      expect(result).toMatch(message);
+    it('basic objcect schema description', () => {
+      const description = generateSchemaDescription(
+        object({
+          height: number().required(),
+          name: string().required(),
+        })
+      );
+      expect(description).toEqual(`{
+  height: number
+  name: string
+}`);
+    });
+
+    it('basic array schema description', () => {
+      const schema = array(string());
+      const description = generateSchemaDescription(schema as any);
+
+      expect(description).toEqual(`[
+string
+]`);
+    });
+
+    it('object array schema description', () => {
+      const schema = array(object({ height: number() }));
+      const description = generateSchemaDescription(schema as any);
+
+      expect(description).toEqual(`[
+  {
+    height: number
+  }
+]`);
+    });
+    it('meta description', () => {
+      const schema = object({
+        height: number().meta({
+          description: 'height of the mountain in meters',
+        }),
+      });
+      const description = generateSchemaDescription(schema as any);
+
+      expect(description).toEqual(`{
+  height: number // height of the mountain in meters
+}`);
     });
   });
 });
